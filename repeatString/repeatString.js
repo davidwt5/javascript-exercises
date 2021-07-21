63377b87387455015c71d1809c626c81008e5461
davidwt5 <davidwtan75@gmail.com>
2021-07-21 12:57:33 +1000

    completed repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, times) {
+    if(times < 0) return "ERROR";
+    let repeatedString = '';
+    for(i=0; i<times; i++) {
+        repeatedString += str;
+    }
 
+    return repeatedString;
 };
 
 module.exports = repeatString;
