79760a71e02f2e182a01a37b53b25a52d52718dc
davidwt5 <davidwtan75@gmail.com>
2021-07-21 14:51:04 +1000

    completed reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reversed = [];
+    for(char of str.split('')) {
+        reversed.unshift(char);
+    }
+    return reversed.join('');
 };
 
 module.exports = reverseString;
