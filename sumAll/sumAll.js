88cc9f8b54ae8f64fb3eb69a8b03abf570a08de9
davidwt5 <davidwtan75@gmail.com>
2021-07-21 15:15:58 +1000

    completed sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let sum = 0;
 
+    if(typeof(a) != "number" || typeof(b) != "number") return "ERROR";
+    if(a < 0 || b < 0) return "ERROR";
+
+    // Sums when a < b
+    if(a < b) {
+        for(let i=a; i<=b; i++) {
+            sum += i;
+        }
+    }
+    // Sums when b < a
+    for(let i=b; i<=a; i++) {
+        sum += i;
+    }
+    
+
+    return sum;
 };
 
 module.exports = sumAll;
