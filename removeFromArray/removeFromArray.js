e2b22830cd5bf4f9b02adf87be3d75d798654c61
davidwt5 <davidwtan75@gmail.com>
2021-07-21 15:04:35 +1000

    completed removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+    for(item of items) {
+        let index = arr.indexOf(item);
+        if(index > -1) {
+            arr.splice(index, 1);
+        }
+    }
 
+    return arr;
 };
 
 module.exports = removeFromArray;
