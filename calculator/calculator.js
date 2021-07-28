d5872a940231abe8f312281e428b2d79bf2d823b
davidwt5 <davidwtan75@gmail.com>
2021-07-28 10:18:44 +1000

    finished calculator.js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+/* All functions assume well-behaved input */
+
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((arr, obj) => arr += obj, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if(arr.length === 0) return [];
+  return arr.reduce((arr, obj) => arr *= obj);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  if(exponent === 0) return 1;
+
+  let result = 1;
+	for(let i=0; i<exponent; i++) {
+    result *= base;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if(n <= 1) return 1;
+
+  let result = 1;
+	for(let i=n; i>1; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 module.exports = {
