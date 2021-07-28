3f8be043acca53f4cae9dc32e42ccf0eec23997d
davidwt5 <davidwtan75@gmail.com>
2021-07-28 11:58:06 +1000

    completed findTheOldest.js

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,35 @@
-const findTheOldest = function() {
-
+const findTheOldest = oldPeople => {
+    // Assume array is not empty
+    return oldPeople.reduce((currentOldestPerson, oldPerson) => {
+        const currentOldestPersonAge = calculateAge(currentOldestPerson);
+        const oldPersonAge = calculateAge(oldPerson);
+        return (oldPersonAge > currentOldestPersonAge) ? oldPerson : currentOldestPerson;
+    });
 };
+
+function calculateAge(oldPerson) {
+    if(!('yearOfDeath' in oldPerson)) {
+        let thisYear = new Date().getFullYear();
+        return thisYear - oldPerson.yearOfBirth;
+    }
+    return oldPerson.yearOfDeath - oldPerson.yearOfBirth;
+}
+
+console.log(findTheOldest([
+    {
+      name: "Carly",
+      yearOfBirth: 2018,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    }
+]));
 
 module.exports = findTheOldest;
