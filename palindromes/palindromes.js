41cbf78bcc6799269d77c8867e7356f989c96a13
davidwt5 <davidwtan75@gmail.com>
2021-07-28 11:23:33 +1000

    completed palindromes.js

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function(str) {
+    if(str.length === 0) return true;
 
+    // Ignoring irrelevant characters and case
+    str = str.toLowerCase();
+    str = str.split('').filter(char => {
+        const regex = new RegExp('[a-z]');
+        return regex.test(char);
+    });
+
+    // Checks Palindrome
+    let i=0, j=str.length-1;
+    while(i < j) {
+        if(str[i] !== str[j]) 
+            return false;
+        i++;
+        j--;
+    }
+    return true;
 };
 
 module.exports = palindromes;
